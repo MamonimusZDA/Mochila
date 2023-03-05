45c7a278aa76b0c41ac3f220f2fb9c70828450f1
—/\/\ㄙμᎤᏂ1Μひㄹ— <jms_alejandro@hotmail.com>
2023-03-05 15:05:33 -0600

    Agregando compatibilidad con funcion "sort"

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+from functools import total_ordering
+
 from pstats import SortKey
 
 from random import \
@@ -19,6 +21,14 @@ def main ( ):
     for inventario in posibles_inventarios:
         print ( inventario.peso_total )
 
+    posibles_inventarios.sort ( )
+
+    print ( '----------------' )
+
+    for inventario in posibles_inventarios:
+        print ( inventario.peso_total )
+
+@total_ordering
 class Inventario:
     def __init__ ( self ):
         self.lista_articulos = list ( )
@@ -35,6 +45,16 @@ class Inventario:
             self.peso_total +=\
                     self.lista_articulos [ i ] *\
                     posibles_articulos [ i ]
+
+    def __eq__(self, other):
+        return self.peso_total == other.peso_total
+
+    def __lt__(self, other):
+        return self.peso_total < other.peso_total
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}"
+                f"({repr(self.lista_articulos)}, {repr(self.peso)})")
 
 def generarListaArticulos ( ):
     lista_articulos = list ( )
