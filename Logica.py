4f50722607c4880b40ee2e11122eff0702f822ae
—/\/\ㄙμᎤᏂ1Μひㄹ— <jms_alejandro@hotmail.com>
2023-03-07 16:15:38 -0600

    Agregando generacion de hijos

--- Logica.py
+++ Logica.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
 from Inventario import Inventario
-from random import uniform as _uniform
+from random import \
+        uniform as _uniform,\
+        randint as _randint
 
 NUMERO_OBJETOS = 20
 
@@ -26,3 +28,34 @@ def generarListaInventarios ( numero_inventarios, posibles_articulos ):
         lista_inventarios.append ( inventario )
 
     return lista_inventarios
+
+def generarHijos(padre_1, padre_2):
+    padre_1_dividido = []
+    padre_2_dividido = []
+
+    padre_1_dividido.append ( padre_1 [ 0 : int ( NUMERO_OBJETOS / 2 ) ] )
+    padre_1_dividido.append ( padre_1 [ int ( NUMERO_OBJETOS / 2 ) : NUMERO_OBJETOS ] )
+    padre_2_dividido.append ( padre_2 [ 0 : int ( NUMERO_OBJETOS / 2 ) ] )
+    padre_2_dividido.append ( padre_2 [ int ( NUMERO_OBJETOS / 2 ) : NUMERO_OBJETOS ] )
+
+    partes_padres = [
+            padre_1_dividido,
+            padre_2_dividido
+    ]
+
+    index_1 = _randint ( 0, 1 )
+    index_2 = _randint ( 0, 1 )
+    index_3 = _randint ( 0, 1 )
+
+    hijos = []
+
+    hijos.append (
+        partes_padres [ index_1 ] [ index_2 ] +\
+        partes_padres [ index_1 - 1 ] [ index_3 ]
+    )
+    hijos.append (
+        partes_padres [ index_1 - 1 ] [ index_2 - 1 ] +\
+        partes_padres [ index_1 ] [ index_3 - 1 ]
+    )
+
+    return hijos
